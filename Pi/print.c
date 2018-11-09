c22265e6b73063a91d116ee05dfc92b0f09a4dca
GiselleSerate <giselle.serate@gmail.com>
2018-11-09 07:59:48 -0800

    Restructure old code and gitignore

--- Pi/print.c
+++ Pi/print.c
@@ -1,5 +1,38 @@
 #include "SPI.h"
 
+// Check if there is something in the Friend's FIFO from the master. 
+// Only allowed to write if we're sent data. 
+short checkFIFO() {
+	
+
+}
+
+// Send back a "reading" from the "ADC" 
+// Currently just the old print code with sendit added. 
+void sendReading(short sendit) {
+	usleep(100000);
+	// Assert TA = 1 to turn on the SPI. CS[7]
+	*SPI0_CS |= 1 << 7;
+	// Send a signal by writing to FIFO. 
+	*SPI0_FIFO = sendit;  
+	// Hang; wait to see if you receive. (Check bit 16 of CS to be 1.)
+	while(!((*SPI0_CS >> 16) & 1)); 
+	// Read from FIFO and save. 
+	char save1 = *SPI0_FIFO;
+	// Send a signal by writing to FIFO. 
+	*SPI0_FIFO = sendit; 
+	// Hang; wait to see if you receive. (Check bit 16 of CS to be 1.)
+	while(!((*SPI0_CS >> 16) & 1));
+	// Read from FIFO and save.
+	char save2 = *SPI0_FIFO;
+
+	// Assemble what you read into one number (0d10 bits; fits in 2 bytes). 
+	short savefull = ((0x3 & save1) << 4) + save2;
+	printf("%#06x\n",savefull);
+	// Assert TA = 0 to turn off the SPI. CS[7]
+	*SPI0_CS &= ~(1 << 7);
+}
+
 int main() {
 	pioInit();
 	spi0Init();
@@ -11,26 +44,7 @@ int main() {
 	// Set clock rate at ~1MHz: clock div=128 if at 250MHz (nearest power of 2). CLK[15:0]
 	*SPI0_CLK = 128;
 	while(1) {
-		usleep(100000);
-		// Assert TA = 1 to turn on the SPI. CS[7]
-		*SPI0_CS |= 1 << 7;
-		// Send a signal by writing to FIFO. 
-		*SPI0_FIFO = 0x7f;  
-		// Hang; wait to see if you receive. (Check bit 16 of CS to be 1.)
-		while(!((*SPI0_CS >> 16) & 1)); 
-		// Read from FIFO and save. 
-		char save1 = *SPI0_FIFO;
-		// Send a signal by writing to FIFO. 
-		*SPI0_FIFO = 0x7f; 
-		// Hang; wait to see if you receive. (Check bit 16 of CS to be 1.)
-		while(!((*SPI0_CS >> 16) & 1));
-		// Read from FIFO and save.
-		char save2 = *SPI0_FIFO;
-
-		// Assemble what you read into one number (0d10 bits; fits in 2 bytes). 
-		short savefull = ((0x3 & save1) << 4) + save2;
-		printf("%#06x\n",savefull);
-		// Assert TA = 0 to turn off the SPI. CS[7]
-		*SPI0_CS &= ~(1 << 7);
+		short id = checkFIFO();
+		sendReading();
 	}
 }
