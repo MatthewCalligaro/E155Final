8751db57b0d62f707ac1465994ca181a6d14baf0
GiselleSerate <giselle.serate@gmail.com>
2018-11-19 22:48:20 -0800

    Add subpar SPI attempts

--- Pi/print.c
+++ Pi/print.c
@@ -11,24 +11,38 @@
 // Only allowed to write if we're sent data. 
 int checkFIFO() {
 	printf("Waiting for SDEP from master...\n");
+	int retVal = 0;
 	while(!digitalRead(IRQ)); // Wait for an SDEP.
-	printf("Received SDEP from master.\n");
-	SPI0CS |= 1 << 7;
-	int retVal = SPI0FIFO;
-	SPI0CS &= ~(1 << 7);
+	while(digitalRead(IRQ)) {
+		printf("Received SDEP from master.\n");
+		//SPI0CS |= 1 << 7;
+		retVal = SPI0FIFO;
+//		if(retVal>0) 
+		printf("%x\n",retVal);
+	//	SPI0CS &= ~(1 << 7);
+
+	}
+	printf("Returning.\n");
+	//SPI0CS &= ~(1 << 7);
 	return retVal;
 }
 
 // Send back a "reading" from the "ADC" 
-void sendReading(long long sendit) {
+void sendReading(unsigned long long sendit0, unsigned long long sendit1) {
 	printf("Writing to SPI.\n");
 	// Assert TA = 1 to turn on the SPI. CS[7]
-	SPI0CS |= 1 << 7;
+//	SPI0CS |= 1 << 7;
 	usleep(100); // Prescribed 100us delay before writing to SPI. 		
 	// Send a signal by writing to FIFO. 
-	SPI0FIFO = sendit;  
+	SPI0FIFO = sendit0;
+	while(!((SPI0CS >> 16) & 1)); 
+	long long save1 = SPI0FIFO;
+	SPI0FIFO = sendit1; // TODO bro this isn't how you SPI at all? I'm p sure.
+	while(!((SPI0CS >> 16) & 1)); 
+	long long save2 = SPI0FIFO;
+	printf("%x %x\n", save1, save2);
 	// Assert TA = 0 to turn off the SPI. CS[7]
-	SPI0CS &= ~(1 << 7);
+	//SPI0CS &= ~(1 << 7);
 }
 
 int main() {
@@ -42,8 +56,13 @@ int main() {
 	// Set clock rate at ~1MHz: clock div=128 if at 250MHz (nearest power of 2). CLK[15:0]
 	SPI0CLK = 128;
 	while(1) {
-		int id = checkFIFO();
-		printf("id: %d\n", id);
-		sendReading(0x20000a02ffff);
+		//int id = checkFIFO();
+	//	printf("id: i%d\n", id);
+		SPI0CS |= 1 << 7;	
+		sendReading(0x101a01fd41542b42,0x4c45554152545458);
+		sendReading(0x100a010c03d00102,0x030405060708090a);
+	//	sendReading(0x41542b424c455541525454583d01020304050607080a);
+		checkFIFO();
+		SPI0CS &= ~(1 << 7);
 	}
 }
