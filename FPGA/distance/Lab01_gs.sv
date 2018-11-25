18b8be813f522da71ee7a523ce9b8a4e43d13c73
Giselle Serate <giselle.serate@gmail.com>
2018-11-24 20:25:25 -0800

    Comment code, add precision
    Not that any of the off by ones are awful, since these are extremely squishy and ultimately qualitative numbers anyway.

--- FPGA/distance/Lab01_gs.sv
+++ FPGA/distance/Lab01_gs.sv
@@ -1,37 +1,34 @@
 // Giselle Serate
-// gserate@g.hmc.edu
-// 2018.09.09
-// Top level module to blink LEDs and set 7seg display 
-// according to passed clock and switches. 
-module Lab01(input logic clk, 			// 40 MHz clock
-				 input logic echo,			// Echo pin
-				 output logic trig,			// Trigger pin
-				 output logic[7:0] led);	// LED bars
+// 2018.11.24
+// Top level module to turn on LED array according to HC-SR04 distance sensor. 
+module Lab01(input logic clk, 			// 40 MHz clock.
+				 input logic echo,			// Echo pin.
+				 output logic trig,			// Trigger pin.
+				 output logic[7:0] led);	// LED bars.
 	logic [5:0] ucount; // Counts up to 1 us; reset when you hit 40. 
-	logic uclk; // Posedge once every us. 
+	logic uclk; // Has a posedge once every us. 
 	logic [15:0] counter; // Counts up; proxy for state. 
 	logic [11:0] accumulateresult; 	// Keeps track of how long echo has been raised. 
 	logic [11:0] save; 					// If we assume a 10-foot max range (as in dsheet), we get 17760 us as our max. 
-												// If instead we assume a use range of 2 feet, we get 3552. 
+												// If instead we assume a use range of 2 feet, we get 3552 us. 
 	
 	// Generate us clock
 	always_ff@(posedge clk)
 	begin
-		if(ucount == 6'd40)
+		if(ucount == 6'd39) // Reset. 
 			ucount = 0;
 		else
 			ucount++;
 			
-		if(ucount % 20 == 0)
-			uclk = !uclk;
+		if(ucount % 20 == 0) // Hit half a us. 
+			uclk = !uclk; 
 	end
 		
 	always_ff@(posedge uclk)
 	begin
-		// Reset on 60 ms or 60000 us
-		if(counter == 16'd60000)
+		// Reset on 60 ms (60000 us)
+		if(counter == 16'd59999)
 		begin
-			led[0] = 1;
 			save = accumulateresult;
 			counter = 0;
 			accumulateresult = 0;
@@ -39,17 +36,15 @@ module Lab01(input logic clk, 			// 40 MHz clock
 		end
 		else
 		begin
-			if(counter == 16'd20) trig = 0; 
-			counter++; // Always do this, regardless of triggering. 
+			if(counter == 16'd19) trig = 0; // Stop triggering; 10 us didn't work, so I bumped this to 20. 
+			counter++; // Regardless of trigger state, continue counting. 
 		end
 			
-		if(echo)
+		if(echo) // Count how long echo is raised. 
 			accumulateresult++;
 	end
-	
-//	assign led[0] = (uclk == 0);
 		
-	always_comb
+	always_comb // Evenly split into intervals of 444. 
 	begin
 		led[7] = (save > 12'd3108);
 		led[6] = (save > 12'd2664);
@@ -58,16 +53,7 @@ module Lab01(input logic clk, 			// 40 MHz clock
 		led[3] = (save > 12'd1332);
 		led[2] = (save > 12'd888);
 		led[1] = (save > 12'd444);
-//		led[0] = (save > 0);
+		led[0] = (save > 0);
 	end
-
-//		assign led[7] = (save > 12'd3108);
-//		assign led[6] = (save > 12'd2664);
-//		assign led[5] = (save > 12'd2220);
-//		assign led[4] = (save > 12'd1776);
-//		assign led[3] = (save > 12'd1332);
-//		assign led[2] = (save > 12'd888);
-//		assign led[1] = (save > 12'd444);
-//		assign led[0] = (save > 0);
 		
 endmodule
