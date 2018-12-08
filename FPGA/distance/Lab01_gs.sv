bfb581a33634b279725f1f165934568abff4cdbb
Giselle Serate <giselle.serate@gmail.com>
2018-12-07 18:32:59 -0800

    Preserve reverty noodling
    Actual reverting would be faster but I can't figure out how to stash in GHD.

--- FPGA/distance/Lab01_gs.sv
+++ FPGA/distance/Lab01_gs.sv
@@ -4,13 +4,13 @@
 // Returns intensity from distance sensor. 
 
 // Ring buffer which always writes and reads on clock. 
-module ring(input logic clk, reset, trig,
+module ring(input logic clk, reset,// trig,
             input logic [11:0] WD,
             output logic [11:0] RD);
               
     logic trigHigh; 
-    logic [2:0] WA;
-    logic [2:0] RA;
+    logic [3:0] WA;
+    logic [3:0] RA;
     logic [11:0] memory[14:0];
     
     always_ff@(posedge clk, posedge reset)
@@ -23,7 +23,7 @@ module ring(input logic clk, reset, trig,
             trigHigh <= 0;
         end
         else
-        if(trig && !trigHigh)
+        //if(trig && !trigHigh)
         begin
             trigHigh <= 1;
             // Read and write data; update addresses. 
@@ -38,7 +38,7 @@ endmodule
 
 // Average current and last 6 data points. 
 module saveavg(input logic clk, reset,
-               input logic trig,
+//               input logic trig,
                input logic[11:0] latest,
                output logic[11:0] avg);
                     
@@ -48,7 +48,7 @@ module saveavg(input logic clk, reset,
     logic [2:0] gettingvalues; // Are we still getting readings for the initial sum?
     
     // Get oldest reading from memory, write newest reading.
-    ring summem(clk, reset, trig, latest, oldest);
+    ring summem(clk, reset, latest, oldest);
     
     always_ff@(posedge clk, posedge reset)
     begin
@@ -59,7 +59,7 @@ module saveavg(input logic clk, reset,
             trigHigh = 0;
         end
         else
-        if(trig && !trigHigh) // Detect positive edges of the trigger signal. 
+//        if(trig && !trigHigh) // Detect positive edges of the trigger signal. 
         begin
             trigHigh = 1;
             if(gettingvalues < 3'd6) gettingvalues++;
@@ -78,17 +78,18 @@ module saveavg(input logic clk, reset,
 endmodule
 
 // Return intensity level according to HC-SR04 distance sensor. 
-module Lab01(input logic clk,                // 40 MHz clock.
+module Lab01(input logic clk,                // 40 MHz clock. // TODO
                 input logic reset,              // Hardware reset. 
                 input logic echo,               // Echo pin.
                 output logic trig,              // Trigger pin.
-                output logic[3:0] intensity);   // Intensity level--higher meaning closer.
+                output logic[7:0] led);   // Intensity level--higher meaning closer. // TODO
 
     logic [5:0] ucount; // Counts up to 1 us; reset when you hit 40. 
     logic uclk; // Has a posedge once every us. 
     logic [15:0] counter; // Counts up once every us; proxy for state. 
-    logic [11:0] accumulateresult;  // Keeps track of how long echo has been raised. 
-    logic [11:0] saveresult;        // save persists the last value while accumulate gets the next value. 
+    logic [11:0] accumulate;  // Keeps track of how long echo has been raised. 
+	 logic [11:0] hold; 
+    logic [11:0] save;        // save persists the last value while accumulate gets the next value. 
                                     // If we assume a use range of 2 feet, we get 3552 us as our max. 
     
     // Generate us clock.
@@ -111,16 +112,16 @@ module Lab01(input logic clk,                // 40 MHz clock.
         // Reset on 60 ms (60000 us).
         if(reset)
         begin
-            saveresult = accumulateresult;
+            hold = accumulate;
             counter = 0;
-            accumulateresult = 0;
+            accumulate = 0;
             trig = 1; // Raise trig, beginning of cycle. 
         end
             else if(counter == 16'd59999)
             begin
-                saveresult = accumulateresult;
+                hold = accumulate;
                 counter = 0;
-                accumulateresult = 0;
+                accumulate = 0;
                 trig = 1; // Raise trig, beginning of cycle. 
             end
         else
@@ -128,26 +129,40 @@ module Lab01(input logic clk,                // 40 MHz clock.
             if(counter == 16'd19) trig = 0; // Stop triggering; 10 us didn't work, so I bumped this to 20. 
             counter++; // Regardless of trigger state, continue counting.             
             if(echo) // Count how long echo is raised. 
-                accumulateresult++;
+                accumulate++;
         end
 
     end
         
     // Apply moving average filter. 
-    saveavg smoother(uclk, reset, trig, hold, save);
-
+    saveavg smoother(trig, reset, hold, save);
+	 // TODO
+	/*
     always_comb // Evenly split 3552us into intervals of 444. 
     begin
         if(saveresult > 12'd3552)           intensity = 3'd0; // Farthest; least intense. 
-        else if(saveresult > 12'd3108)      intensity = 3'd1; 
-        else if(saveresult > 12'd2664)      intensity = 3'd2;
-        else if(saveresult > 12'd2220)      intensity = 3'd3;
-        else if(saveresult > 12'd1776)      intensity = 3'd4;
-        else if(saveresult > 12'd1332)      intensity = 3'd5;
-        else if(saveresult > 12'd888)       intensity = 3'd6;
-        else if(saveresult > 12'd444)       intensity = 3'd7;
-        else if(saveresult > 0)             intensity = 3'd8; // Closest; most intense.
+        else if(save > 12'd3108)      intensity = 3'd1; 
+        else if(save > 12'd2664)      intensity = 3'd2;
+        else if(save > 12'd2220)      intensity = 3'd3;
+        else if(save > 12'd1776)      intensity = 3'd4;
+        else if(save > 12'd1332)      intensity = 3'd5;
+        else if(save > 12'd888)       intensity = 3'd6;
+        else if(save > 12'd444)       intensity = 3'd7;
+        else if(save > 0)             intensity = 3'd8; // Closest; most intense.
         else                                intensity = 3'd0; // If we see a value that we don't know how to handle, read a 0. 
+    end
+	*/
+	// Set LEDs according to latest distance reading. 
+    always_comb // Split into intervals of 444. 
+    begin
+        led[7] = (save > 12'd3108);     // Indicates furthest distance. 
+        led[6] = (save > 12'd2664);
+        led[5] = (save > 12'd2220);
+        led[4] = (save > 12'd1776);
+        led[3] = (save > 12'd1332);
+        led[2] = (save > 12'd888);
+        led[1] = (save > 12'd444);
+        led[0] = (save > 0);            // Indicates closest distance. 
     end
     
 endmodule
