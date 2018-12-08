511dede955baa02b7532d2e3abe7ae9b56578d4f
Giselle Serate <giselle.serate@gmail.com>
2018-12-07 18:35:56 -0800

    Resolve merge conflicts from revert

--- FPGA/distance/Lab01_gs.sv
+++ FPGA/distance/Lab01_gs.sv
@@ -1,7 +1,8 @@
 // Giselle Serate
 // gserate@g.hmc.edu
 // 2018.11.30
-// Returns intensity from distance sensor. 
+// Sets LED bars according to the distance read from HC-SR04 ultrasonic sensor. 
+// Applies boxcar average filter. 
 
 // Ring buffer which always writes and reads on clock. 
 module ring(input logic clk, reset,// trig,
@@ -77,82 +78,63 @@ module saveavg(input logic clk, reset,
 
 endmodule
 
-// Return intensity level according to HC-SR04 distance sensor. 
-module Lab01(input logic clk,                // 40 MHz clock. // TODO
-                input logic reset,              // Hardware reset. 
-                input logic echo,               // Echo pin.
-                output logic trig,              // Trigger pin.
-                output logic[7:0] led);   // Intensity level--higher meaning closer. // TODO
-
-    logic [5:0] ucount; // Counts up to 1 us; reset when you hit 40. 
-    logic uclk; // Has a posedge once every us. 
-    logic [15:0] counter; // Counts up once every us; proxy for state. 
-    logic [11:0] accumulate;  // Keeps track of how long echo has been raised. 
-	 logic [11:0] hold; 
-    logic [11:0] save;        // save persists the last value while accumulate gets the next value. 
-                                    // If we assume a use range of 2 feet, we get 3552 us as our max. 
+// Top level module that turns on elements of an LED array according to HC-SR04 distance sensor. 
+module Lab01(input logic clk,           // 40 MHz clock.
+                input logic echo,          // Echo pin.
+                output logic trig,         // Trigger pin.
+                output logic[7:0] led);    // LED bars.
+    // Track clock and state.
+    logic [5:0] ucount;     // Counts up to 1 us; reset when you hit 40. 
+    logic uclk;             // Has a posedge once every us. 
+    logic [15:0] counter;   // Counts up once per us; proxy for state. 
+    
+    // Track how long echo has been raised. 
+    logic [11:0] accumulateresult;  // Gets the next sensor value.
+    logic [11:0] hold;              // Persists the last sensor value.
+     logic [11:0] clean[14:0];          // Save the last 10 values.
+     logic [11:0] save;                     // Hold actual result.
+                                    // Assuming a use range of 2 feet, the maximum is 3552 us.
+    
+    integer i; // For loop var. It's a shift reg, so is legit?
     
     // Generate us clock.
-    always_ff@(posedge clk, posedge reset)
+    always_ff@(posedge clk)
     begin
-        if(reset) // Reset. 
-            ucount = 0;
-        else if (ucount == 6'd39) // Also reset. 
+        if(ucount == 6'd39) // Reset. 
             ucount = 0;
         else
-        begin
             ucount++;
-            if(ucount % 20 == 0) // Hit half a us. 
-                uclk = !uclk; 
-        end
+            
+        if(ucount % 20 == 0) // Half a us has passed; flip clock. 
+            uclk = !uclk; 
     end
         
-    always_ff@(posedge uclk, posedge reset)
+    // Communicate with sensor. 
+    always_ff@(posedge uclk)
     begin
         // Reset on 60 ms (60000 us).
-        if(reset)
+        if(counter == 16'd59999)
         begin
-            hold = accumulate;
+           hold = accumulateresult;
             counter = 0;
             accumulate = 0;
             trig = 1; // Raise trig, beginning of cycle. 
         end
-            else if(counter == 16'd59999)
-            begin
-                hold = accumulate;
-                counter = 0;
-                accumulate = 0;
-                trig = 1; // Raise trig, beginning of cycle. 
-            end
         else
         begin
-            if(counter == 16'd19) trig = 0; // Stop triggering; 10 us didn't work, so I bumped this to 20. 
-            counter++; // Regardless of trigger state, continue counting.             
-            if(echo) // Count how long echo is raised. 
-                accumulate++;
+            if(counter == 16'd19) trig = 0; // Stop triggering; the stated minimum of
+                                            // 10 us didn't work, but 20 did.
+            counter++; // Regardless of trigger state, continue counting. 
         end
-
+            
+        if(echo) // Count how long echo is raised. 
+            accumulateresult++;
     end
-        
-    // Apply moving average filter. 
+     
+     // Apply moving average filter. 
     saveavg smoother(trig, reset, hold, save);
-	 // TODO
-	/*
-    always_comb // Evenly split 3552us into intervals of 444. 
-    begin
-        if(saveresult > 12'd3552)           intensity = 3'd0; // Farthest; least intense. 
-        else if(save > 12'd3108)      intensity = 3'd1; 
-        else if(save > 12'd2664)      intensity = 3'd2;
-        else if(save > 12'd2220)      intensity = 3'd3;
-        else if(save > 12'd1776)      intensity = 3'd4;
-        else if(save > 12'd1332)      intensity = 3'd5;
-        else if(save > 12'd888)       intensity = 3'd6;
-        else if(save > 12'd444)       intensity = 3'd7;
-        else if(save > 0)             intensity = 3'd8; // Closest; most intense.
-        else                                intensity = 3'd0; // If we see a value that we don't know how to handle, read a 0. 
-    end
-	*/
-	// Set LEDs according to latest distance reading. 
+        
+    // Set LEDs according to latest distance reading. 
     always_comb // Split into intervals of 444. 
     begin
         led[7] = (save > 12'd3108);     // Indicates furthest distance. 
@@ -164,5 +146,5 @@ module Lab01(input logic clk,                // 40 MHz clock. // TODO
         led[1] = (save > 12'd444);
         led[0] = (save > 0);            // Indicates closest distance. 
     end
-    
+
 endmodule
