e0c31b2ccb9ff00e0b7ff7587d5d019d8b62c138
GiselleSerate <giselle.serate@gmail.com>
2018-12-07 23:42:46 -0800

    Add module that's been escaping version control
    Angery reax.

--- FPGA/distance.sv
+++ FPGA/distance.sv
@@ -32,41 +32,57 @@ module ring(input logic clk, reset,
 
 endmodule
 
+
 // Average current and last 6 data points. 
 module saveavg(input logic clk, reset,
+               input logic trig,
                input logic[11:0] latest,
                output logic[11:0] avg);
                     
+    logic trigHigh; // If we expect trig to currently be high or not. 
     logic [11:0] oldest; // Oldest reading saved in memory. 
     logic [15:0] sum; // Saved sum across runs; used to calculate average. 
     logic [2:0] gettingvalues; // Are we still getting readings for the initial sum?
     
     // Get oldest reading from memory, write newest reading.
-    ring summem(clk, reset, latest, oldest);
+    ring summem(clk, reset, trig, latest, oldest);
     
     always_ff@(posedge clk, posedge reset)
     begin
         if(reset)
         begin
-            sum = 15'd24864; // Leave sensor staring at infinity for about a third of a second before messing with it. 
+            sum = 15'd24864; // Initialize at far distance (no effects). 
             gettingvalues = 0;
+            trigHigh = 0;
         end
         else
+        if(!trigHigh) // Waiting for trig to go high. 
         begin
-            if(gettingvalues < 3'd6) gettingvalues++;
-            else
+            if(trig) // Trig went high. Handle. 
             begin
-                // Update sum. 
-                sum -= oldest;
-                sum += latest;
+                trigHigh = 1;
+                if(gettingvalues <= 3'd6) 
+                begin
+                    gettingvalues++;
+                    sum -= 12'd3552;
+                    sum += latest;
+                end
+                else
+                begin
+                    // Update sum. 
+                    sum -= oldest;
+                    sum += latest;
+                end
             end
         end
+        else trigHigh = trig; // Trig is high; idle until trig is low again. 
     end
     
     // Calculate average of seven points. 
-    assign avg = sum / 4'd7;
+    assign avg = sum / 4'd7; 
 
 endmodule
+
 
 // Return intensity level according to HC-SR04 distance sensor. 
 module distance(input logic clk,                // 40 MHz clock.
@@ -74,7 +90,8 @@ module distance(input logic clk,                // 40 MHz clock.
                 input logic echo,               // Echo pin.
                 output logic trig,              // Trigger pin.
                 output logic[3:0] intensity);   // Intensity level--higher meaning closer.
-
+    
+    // Track clock and state.
     logic [5:0] ucount; // Counts up to 1 us; reset when you hit 40. 
     logic uclk; // Has a posedge once every us. 
     logic [15:0] counter; // Counts up once every us; proxy for state. 
@@ -87,57 +104,64 @@ module distance(input logic clk,                // 40 MHz clock.
     begin
         if(reset) // Reset. 
             ucount = 0;
-        else if (ucount == 6'd39) // Also reset. 
-            ucount = 0;
         else
         begin
-            ucount++;
-            if(ucount % 20 == 0) // Hit half a us. 
+            if(ucount == 6'd39) // Reset. 
+                ucount = 0;
+            else
+                ucount++;
+                
+            if(ucount % 20 == 0) // Half a us has passed; flip clock. 
                 uclk = !uclk; 
         end
     end
         
+    // Communicate with sensor. 
     always_ff@(posedge uclk, posedge reset)
     begin
-        // Reset on 60 ms (60000 us).
         if(reset)
         begin
-            saveresult = accumulateresult;
+            hold = accumulateresult;
             counter = 0;
             accumulateresult = 0;
             trig = 1; // Raise trig, beginning of cycle. 
         end
-            else if(counter == 16'd59999)
+        else
+        begin
+            // Reset on 60 ms (60000 us).
+            if(counter == 16'd59999)
             begin
-                saveresult = accumulateresult;
+                hold = accumulateresult;
                 counter = 0;
                 accumulateresult = 0;
                 trig = 1; // Raise trig, beginning of cycle. 
             end
-        else
-        begin
-            if(counter == 16'd19) trig = 0; // Stop triggering; 10 us didn't work, so I bumped this to 20. 
-            counter++; // Regardless of trigger state, continue counting.             
-            if(echo) // Count how long echo is raised. 
-                accumulateresult++;
+            else
+            begin
+                if(counter == 16'd19) trig = 0; // Stop triggering; the stated minimum of
+                                                // 10 us didn't work, but 20 did.
+                counter++; // Regardless of trigger state, continue counting. 
+                if(echo) // Count how long echo is raised. 
+                    accumulateresult++;
+            end
         end
 
     end
-        
-    // Apply moving average filter. 
-    saveavg smoother(trig, reset, hold, save);
+     
+     // Apply moving average filter. 
+    saveavg smoother(clk, reset, trig, hold, save);
 
     always_comb // Evenly split 3552us into intervals of 444. 
     begin
-        if(saveresult > 12'd3552)           intensity = 3'd0; // Farthest; least intense. 
-        else if(saveresult > 12'd3108)      intensity = 3'd1; 
-        else if(saveresult > 12'd2664)      intensity = 3'd2;
-        else if(saveresult > 12'd2220)      intensity = 3'd3;
-        else if(saveresult > 12'd1776)      intensity = 3'd4;
-        else if(saveresult > 12'd1332)      intensity = 3'd5;
-        else if(saveresult > 12'd888)       intensity = 3'd6;
-        else if(saveresult > 12'd444)       intensity = 3'd7;
-        else if(saveresult > 0)             intensity = 3'd8; // Closest; most intense.
+        if(save > 12'd3552)           intensity = 3'd0; // Farthest; least intense. 
+        else if(save > 12'd3108)      intensity = 3'd1; 
+        else if(save > 12'd2664)      intensity = 3'd2;
+        else if(save > 12'd2220)      intensity = 3'd3;
+        else if(save > 12'd1776)      intensity = 3'd4;
+        else if(save > 12'd1332)      intensity = 3'd5;
+        else if(save > 12'd888)       intensity = 3'd6;
+        else if(save > 12'd444)       intensity = 3'd7;
+        else if(save > 0)             intensity = 3'd8; // Closest; most intense.
         else                                intensity = 3'd0; // If we see a value that we don't know how to handle, read a 0. 
     end
     
